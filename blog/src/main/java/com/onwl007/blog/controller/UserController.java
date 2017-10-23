30e4838eb54694eb2120f1a68a1d0ee45c9eb52c
onwl007 <onwl007@126.com>
2017-10-23 22:42:16 +0800

    修改查询所有用户

--- blog/src/main/java/com/onwl007/blog/controller/UserController.java
+++ blog/src/main/java/com/onwl007/blog/controller/UserController.java
@@ -9,16 +9,21 @@ import com.onwl007.blog.util.ConstraintViolationExceptionHandler;
 import com.onwl007.blog.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.ConstraintViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
 public class UserController {
 
     @Autowired
@@ -40,10 +45,18 @@ public class UserController {
      * @return
      */
     @GetMapping()
-    public ModelAndView list(Model model){
-        model.addAttribute("userList",userRepository.findAll());
-        model.addAttribute("title","用户管理");
-        return new ModelAndView("users/list","userModel",model);
+    public ModelAndView list(@RequestParam(value = "async",required = false)boolean async,
+                             @RequestParam(value = "pageIndex",required = false,defaultValue = "0")int pageIndex,
+                             @RequestParam(value = "pageSize",required = false,defaultValue = "10")int pageSize,
+                             @RequestParam(value = "name",required = false,defaultValue = "")String name,Model model){
+
+        Pageable pageable=new PageRequest(pageIndex,pageSize);
+        Page<User> page=userService.listUsersByNameLike(name,pageable);
+        List<User> list=page.getContent(); //当前所在页面数据列表
+
+        model.addAttribute("page",page);
+        model.addAttribute("userList",list);
+        return new ModelAndView(async==true?"users/list :: #mainContainerRepleace":"users/list", "userModel", model);
     }
 
     /**
