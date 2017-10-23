0647e7e6a83eee41622d5cf2851bcacc58d2d015
onwl007 <onwl007@126.com>
2017-10-23 22:41:43 +0800

    增加菜单

--- blog/src/main/java/com/onwl007/blog/controller/AdminController.java
+++ blog/src/main/java/com/onwl007/blog/controller/AdminController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.onwl007.blog.vo.Menu;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 后台管理控制器
  */
@@ -20,7 +24,12 @@ public class AdminController {
      */
     @GetMapping
     public ModelAndView listUsers(Model model){
-        return new ModelAndView("admins/index","menulist",model);
+        List<Menu> list=new ArrayList<>();
+        list.add(new Menu("用户管理","/users"));
+        list.add(new Menu("角色管理","/roles"));
+        list.add(new Menu("博客管理","/blogs"));
+        list.add(new Menu("评论管理","commits"));
+        model.addAttribute("list",list);
+        return new ModelAndView("/admins/index","model",model);
     }
-
 }
