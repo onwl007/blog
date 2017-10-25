2ca5689f94e0bb99823559884250739c159307f1
onwl007 <onwl007@126.com>
2017-10-25 22:25:37 +0800

    实现博客服务

--- blog/src/main/java/com/onwl007/blog/service/BlogServiceImpl.java
+++ blog/src/main/java/com/onwl007/blog/service/BlogServiceImpl.java
@@ -1,77 +1,142 @@
 package com.onwl007.blog.service;
 
-import com.onwl007.blog.domain.Blog;
-import com.onwl007.blog.domain.Catalog;
-import com.onwl007.blog.domain.User;
+import com.onwl007.blog.domain.*;
+import com.onwl007.blog.domain.es.EsBlog;
 import com.onwl007.blog.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * blog服务
  */
+@Service
 public class BlogServiceImpl implements BlogService{
 
     @Autowired
     private BlogRepository blogRepository;
-    //@Autowired
-    //private EsBlogService esBlogService;
+    @Autowired
+    private EsBlogService esBlogService;
 
+    /**
+     * 保存博客，同时保存ES
+     * @param blog
+     * @return
+     */
     @Transactional
     @Override
     public Blog saveBlog(Blog blog) {
-        return null;
+        boolean isNew=(blog.getId()==null);
+        EsBlog esBlog=null;
+        Blog returnBlog=blogRepository.save(blog);
+
+        if (isNew){
+            esBlog =new EsBlog(returnBlog);
+        }else {
+            esBlog=esBlogService.getEsBlogByBlogId(blog.getId());
+            esBlog.update(returnBlog);
+        }
+
+        esBlogService.updateEsBlog(esBlog);
+        return returnBlog;
     }
 
+    /**
+     * 删除博客，同时删除掉ES里的
+     * @param id
+     */
+    @Transactional
     @Override
     public void removeBlog(Long id) {
-
+        blogRepository.delete(id);
+        EsBlog esBlog=esBlogService.getEsBlogByBlogId(id);
+        esBlogService.removeEsBlog(esBlog.getId());
     }
 
     @Override
     public Blog getBlogById(Long id) {
-        return null;
+        return blogRepository.findOne(id);
     }
 
+    /**
+     * 根据文章标题模糊查询
+     * @param user
+     * @param title
+     * @param pageable
+     * @return
+     */
     @Override
     public Page<Blog> listBlogsByTitleVote(User user, String title, Pageable pageable) {
-        return null;
+        //模糊查询
+        title="%"+title+"%";
+        String tags=title;
+        Page<Blog> blogs=blogRepository.findByTitleLikeAndUserOrTagsLikeAndUserOrderByCreateTimeDesc(title,user,tags,user,pageable);
+        return blogs;
     }
 
     @Override
-    public Page<Blog> listBlogsByTitleVoteAndSort(User suser, String title, Pageable pageable) {
-        return null;
+    public Page<Blog> listBlogsByTitleVoteAndSort(User user, String title, Pageable pageable) {
+        //模糊查询
+        title="%"+title+"%";
+        Page<Blog> blogs=blogRepository.findByUserAndTitleLike(user,title,pageable);
+        return blogs;
     }
 
+    /**
+     *根据分类查询
+     * @param catalog
+     * @param pageable
+     * @return
+     */
     @Override
     public Page<Blog> listBlogsByCatalog(Catalog catalog, Pageable pageable) {
-        return null;
+        Page<Blog> blogs=blogRepository.findByCatalog(catalog,pageable);
+        return blogs;
     }
 
     @Override
     public void readingIncrease(Long id) {
+        Blog blog=blogRepository.findOne(id);
+        blog.setReadSize(blog.getCommentSize()+1);
+        this.saveBlog(blog);
 
     }
 
     @Override
     public Blog createComment(Long blogId, String commentContent) {
-        return null;
+        Blog originBlog=blogRepository.findOne(blogId);
+        User user=(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Comment comment=new Comment(user,commentContent);
+        originBlog.addComment(comment);
+        return this.saveBlog(originBlog);
     }
 
     @Override
     public void removeComment(Long blogId, Long commentId) {
-
+        Blog originBlog=blogRepository.findOne(blogId);
+        originBlog.removeComment(commentId);
+        this.saveBlog(originBlog);
     }
 
     @Override
     public Blog createVote(Long blogId) {
-        return null;
+        Blog originBlog=blogRepository.findOne(blogId);
+        User user=(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Vote vote=new Vote(user);
+        boolean isExist=originBlog.addVote(vote);
+        if (isExist){
+            throw new IllegalArgumentException("该用户已经点过赞了");
+        }
+        return this.saveBlog(originBlog);
     }
 
     @Override
     public void removeVote(Long blogId, Long voteId) {
-
+        Blog originBlog=blogRepository.findOne(blogId);
+        originBlog.removeVote(voteId);
+        this.saveBlog(originBlog);
     }
 }
