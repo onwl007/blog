5cbe1bf8fbbfd4b0e0e2430b544d8834b10124c6
onwl007 <onwl007@126.com>
2018-03-07 23:46:51 +0800

    完善

--- blog/src/main/java/com/onwl007/blog/domain/ResultCode.java
+++ blog/src/main/java/com/onwl007/blog/domain/ResultCode.java
@@ -1,12 +1,5 @@
 package com.onwl007.blog.domain;
 
-/**
- * @author onwl007@126.com
- * @date 2018/3/7 21:23
- */
-/**
- * 响应码枚举，参考HTTP状态码的语义
- */
 public enum ResultCode {
     SUCCESS(200),//成功
     FAIL(400),//失败
@@ -14,13 +7,13 @@ public enum ResultCode {
     NOT_FOUND(404),//接口不存在
     INTERNAL_SERVER_ERROR(500);//服务器内部错误
 
-    private final int code;
+    private int code;
 
-    ResultCode(int code) {
+    private ResultCode(int code) {
         this.code = code;
     }
 
-    public int code() {
-        return code;
+    public int getCode() {
+        return this.code;
     }
 }
