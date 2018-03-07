0bda0704d1aa428bab07675301b230fe310150d4
onwl007 <onwl007@126.com>
2018-03-07 21:30:20 +0800

    响应结果生成工具

---
+++ blog/src/main/java/com/onwl007/blog/util/ResultGenerator.java
@@ -0,0 +1,32 @@
+package com.onwl007.blog.util;
+
+import com.onwl007.blog.domain.Result;
+import com.onwl007.blog.domain.ResultCode;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/3/7 21:24
+ * 响应结果生成工具
+ */
+public class ResultGenerator {
+    private static final String DEFAULT_SUCCESS_MESSAGE = "SUCCESS";
+
+    public static Result genSuccessResult() {
+        return new Result()
+                .setCode(ResultCode.SUCCESS)
+                .setMessage(DEFAULT_SUCCESS_MESSAGE);
+    }
+
+    public static Result genSuccessResult(Object data) {
+        return new Result()
+                .setCode(ResultCode.SUCCESS)
+                .setMessage(DEFAULT_SUCCESS_MESSAGE)
+                .setData(data);
+    }
+
+    public static Result genFailResult(String message) {
+        return new Result()
+                .setCode(ResultCode.FAIL)
+                .setMessage(message);
+    }
+}
