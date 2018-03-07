bd52ccba1898f6758a1a512c81f3163a6b6dba10
onwl007 <onwl007@126.com>
2018-03-07 23:46:10 +0800

    修改返回的数据为 json 格式

--- blog/src/main/java/com/onwl007/blog/controller/BlogController.java
+++ blog/src/main/java/com/onwl007/blog/controller/BlogController.java
@@ -1,8 +1,12 @@
 package com.onwl007.blog.controller;
 
+import com.onwl007.blog.domain.Blog;
+import com.onwl007.blog.domain.RestResult;
 import com.onwl007.blog.domain.User;
 import com.onwl007.blog.domain.es.EsBlog;
+import com.onwl007.blog.service.BlogService;
 import com.onwl007.blog.service.EsBlogService;
+import com.onwl007.blog.util.ResultGenerator;
 import com.onwl007.blog.vo.TagVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,65 +18,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * blog控制器
  */
-@Controller
+@RestController
 @RequestMapping("blogs")
 public class BlogController {
 
     @Autowired
     private EsBlogService esBlogService;
 
+    @Autowired
+    private BlogService blogService;
+
+    private ResultGenerator generator;
+
+    /**
+     * 查询全部博客文章并按照时间降序排列
+     * @return
+     */
     @GetMapping
-    public String listBlogs(@RequestParam(value = "order", required = false, defaultValue = "new") String order,
-                            @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
-                            @RequestParam(value = "async", required = false) boolean async,
-                            @RequestParam(value = "pageIndex", required = false, defaultValue = "0") int pageIndex,
-                            @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize,
-                            Model model) {
-
-        Page<EsBlog> page = null;
-        List<EsBlog> list = null;
-        boolean isEmpty = true;//系统初始化时，没有博客数据
-        try {
-            if (order.equals("hot")) {//最热查询
-                Sort sort = new Sort(Sort.Direction.DESC, "readSize", "commentSize", "voteSize", "createTime");
-                Pageable pageable = new PageRequest(pageIndex, pageSize, sort);
-                page = esBlogService.listHotestEsBlogs(keyword, pageable);
-            } else if (order.equals("new")) {//最新查询
-                Sort sort = new Sort(Sort.Direction.DESC, "createTime");
-                Pageable pageable = new PageRequest(pageIndex, pageSize, sort);
-                page = esBlogService.listNewestEsBlogs(keyword, pageable);
-            }
-            isEmpty = false;
-        } catch (Exception e) {
-            Pageable pageable = new PageRequest(pageIndex, pageSize);
-            page = esBlogService.listEsBlogs(pageable);
+    public RestResult listBlogs(){
+        List<Blog> blogList=blogService.listBlogsByCreatTimeDsec();
+        if (blogList!=null&&blogList.size()>0){
+            return generator.getSuccessResult("查询全部博客文章成功",blogList);
         }
-        list = page.getContent();//当前所在页面数据列表
-
-        model.addAttribute("order", order);
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("page", page);
-        model.addAttribute("blogList", list);
-
-        //首次访问页面才加载
-        if (!async && !isEmpty) {
-            List<EsBlog> newest = esBlogService.listTop5NewestEsBlogs();
-            model.addAttribute("newest", newest);
-            List<EsBlog> hotest = esBlogService.listTop5HotestEsBlogs();
-            model.addAttribute("hotest", hotest);
-            List<TagVO> tags = esBlogService.listTop30Tags();
-            model.addAttribute("tags", tags);
-            List<User> users = esBlogService.listTop12Users();
-            model.addAttribute("users", users);
-        }
-
-        return (async == true ? "/index::#mainContainerRepleace" : "/index");
+        return generator.getFailResult("查询全部博客文章失败");
     }
 
     @GetMapping("/newest")
