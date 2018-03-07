9727ad13c35e890ec5b433b05b8bfe988af0eded
onwl007 <onwl007@126.com>
2018-03-07 23:48:03 +0800

    完善返回 json 的格式

--- blog/src/main/java/com/onwl007/blog/util/ResultGenerator.java
+++ blog/src/main/java/com/onwl007/blog/util/ResultGenerator.java
@@ -2,31 +2,56 @@ package com.onwl007.blog.util;
 
 import com.onwl007.blog.domain.RestResult;
 import com.onwl007.blog.domain.ResultCode;
+import org.springframework.stereotype.Component;
+
 
 /**
- * @author onwl007@126.com
- * @date 2018/3/7 21:24
- * 响应结果生成工具
+ * 工厂模式
+ * 接口信息生成工具
+ * 。@Component 添加到Spring组件中
  */
+@Component
 public class ResultGenerator {
-    private static final String DEFAULT_SUCCESS_MESSAGE = "SUCCESS";
 
-    public static RestResult genSuccessResult() {
+    private static final String SUCCESS = "success";
+    //成功
+    public RestResult getSuccessResult() {
         return new RestResult()
                 .setCode(ResultCode.SUCCESS)
-                .setMessage(DEFAULT_SUCCESS_MESSAGE);
+                .setMessage(SUCCESS);
     }
-
-    public static RestResult genSuccessResult(Object data) {
+    //成功，附带额外数据
+    public  RestResult getSuccessResult(Object data) {
         return new RestResult()
                 .setCode(ResultCode.SUCCESS)
-                .setMessage(DEFAULT_SUCCESS_MESSAGE)
+                .setMessage(SUCCESS)
                 .setData(data);
     }
-
-    public static RestResult genFailResult(String message) {
+    //成功，自定义消息及数据
+    public  RestResult getSuccessResult(String message,Object data) {
+        return new RestResult()
+                .setCode(ResultCode.SUCCESS)
+                .setMessage(message)
+                .setData(data);
+    }
+    //失败，附带消息
+    public  RestResult getFailResult(String message) {
         return new RestResult()
                 .setCode(ResultCode.FAIL)
                 .setMessage(message);
+    }
+    //失败，自定义消息及数据
+    public RestResult getFailResult(String message, Object data) {
+        return new RestResult()
+                .setCode(ResultCode.FAIL)
+                .setMessage(message)
+                .setData(data);
+    }
+    //自定义创建
+    public RestResult getFreeResult(ResultCode code, String message, Object data) {
+        return new RestResult()
+                .setCode(code)
+                .setMessage(message)
+                .setData(data);
     }
 }
