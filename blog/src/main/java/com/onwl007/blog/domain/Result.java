ca346b0b1d1d818ddf3e063287adfbeeecfef2c5
onwl007 <onwl007@126.com>
2018-03-07 21:30:33 +0800

    响应结果实体

---
+++ blog/src/main/java/com/onwl007/blog/domain/Result.java
@@ -0,0 +1,45 @@
+package com.onwl007.blog.domain;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/3/7 21:17
+ */
+public class Result {
+    private int code;
+    private String message;
+    private Object data;
+
+    public Result setCode(ResultCode resultCode) {
+        this.code = resultCode.code();
+        return this;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Result setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public Result setData(Object data) {
+        this.data = data;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}
